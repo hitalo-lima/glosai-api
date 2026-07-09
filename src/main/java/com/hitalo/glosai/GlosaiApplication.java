375a3cfd643649e938b1098f9aab2aadf5440204
hitalo-lima <hitalolima173@gmail.com>
2026-07-09 16:45:18 -0300

    inicialização do projeto springboot

---
+++ src/main/java/com/hitalo/glosai/GlosaiApplication.java
@@ -0,0 +1,13 @@
+package com.hitalo.glosai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlosaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlosaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hitalo/glosai/GlosaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitalo.glosai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlosaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
